b3e9e8c3f5400f60de973cd670dd06564b2759d7
Ruckshan Ratnam <ruckshan@Ruckshans-MacBook-Pro.local>
2019-10-29 13:03:15 -0400

    finished master collater

--- master_db_collater.py
+++ master_db_collater.py
@@ -48,3 +48,68 @@ for i in steamData:
 
 # Union all the sets together and turn into a list and sort in alph order
 all_games_alph = (list(set().union(gog_games,game_billet_games,humble_bundle_games,mac_store_games,steam_games))).sort()
+
+
+# Add all games to the master database
+master_database_connection = sqlite3.connect('master_database.db')
+master_db_cursor = master_database_connection.cursor()
+master_db_cursor.execute("CREATE TABLE IF NOT EXISTS prices(gameName TEXT, gogPrice TEXT, gogLink TEXT, macPrice TEXT, macLink TEXT, billetPrice TEXT, billetLink TEXT, humblePrice TEXT, humbleLink TEXT, steamPrice TEXT, steamLink TEXT)")
+master_db_cursor.execute("INSERT INTO prices VALUES('gameName','0.00','https...','0.00','https...','0.00','https...','0.00','https...','0.00','https...')")
+master_database_connection.commit()
+
+sql_select_query = """SELECT * FROM prices WHERE gameName = ?"""
+print("started")
+for i in all_games_alph:
+    gogCursor.execute(sql_select_query, (i,))
+    billetCursor.execute(sql_select_query, (i,))
+    humbleCursor.execute(sql_select_query, (i,))
+    macCursor.execute(sql_select_query, (i,))
+    steamCursor.execute(sql_select_query, (i,))
+
+    gog_name = gogCursor.fetchone()
+    mac_name = macCursor.fetchone()
+    billet_name = billetCursor.fetchone()
+    humble_name = humbleCursor.fetchone()
+    steam_name = steamCursor.fetchone()
+    
+    master_db_cursor.execute("INSERT INTO prices (gameName, gogPrice, gogLink, macPrice, macLink, billetPrice, billetLink, humblePrice, humbleLink, steamPrice, steamLink) VALUES (?,?,?,?,?,?,?,?,?,?,?)",(i, "none", "none", "none", "none", "none","none", "none", "none", "none", "none", "none"))
+    master_database_connection.commit()
+
+    if gog_name is not None:
+        master_db_cursor.execute("UPDATE prices SET gogPrice = ? WHERE gameName = ?",(gog_name[2],i))
+        master_db_cursor.execute("UPDATE prices SET gogLink = ? WHERE gameName = ?",(gog_name[1],i))
+        master_database_connection.commit()
+    if mac_name is not None:
+        master_db_cursor.execute("UPDATE prices SET macPrice = ? WHERE gameName = ?",(mac_name[2],i))
+        master_db_cursor.execute("UPDATE prices SET macLink = ? WHERE gameName = ?",(mac_name[1],i))
+        master_database_connection.commit()
+    if billet_name is not None:
+        master_db_cursor.execute("UPDATE prices SET billetPrice = ? WHERE gameName = ?",(billet_name[2],i))
+        master_db_cursor.execute("UPDATE prices SET billetLink = ? WHERE gameName = ?",(billet_name[1],i))
+        master_database_connection.commit()
+    if humble_name is not None:
+        master_db_cursor.execute("UPDATE prices SET humblePrice = ? WHERE gameName = ?",(humble_name[2],i))
+        master_db_cursor.execute("UPDATE prices SET humbleLink = ? WHERE gameName = ?",(humble_name[1],i))
+        master_database_connection.commit()
+    if steam_name is not None:
+        master_db_cursor.execute("UPDATE prices SET steamPrice = ? WHERE gameName = ?",(steam_name[2],i))
+        master_db_cursor.execute("UPDATE prices SET steamLink = ? WHERE gameName = ?",(steam_name[1],i))
+        master_database_connection.commit()
+
+# Close all cursors
+master_db_cursor.close()
+gogCursor.close()
+billetCursor.close()
+humbleCursor.close()
+macCursor.close()
+steamCursor.close()
+
+# Close all connections 
+master_database_connection.close()
+gog.close()
+gameBillet.close()
+humble.close()
+macStore.close()
+steam.close()
+
+print("done")
