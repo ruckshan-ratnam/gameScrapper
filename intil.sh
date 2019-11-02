809be806c735038d24f6f608d73abca41bcc5d06
Ruckshan Ratnam <ruckshan@Ruckshans-MacBook-Pro.local>
2019-11-02 18:52:10 -0400

    added a script to make life easy

--- intil.sh
+++ intil.sh
@@ -19,3 +19,9 @@ echo "launching scrappers"
 python3 gamebillet_scrapper.py & python3 gog_scrapper.py & python3 humble_bundle_scrapper.py & python3 macgamestore_scrapper.py & python3 steam_store_scrapper.py
 
 echo "done scrapping"
+
+echo "moving databses and scrapping files to database and scrapping folder"
+
+mkdir database_scrapping
+mv -t database_scrapping gamebillet_scrapper.py gog_scrapper.py humble_bundle_scrapper.py info_collater.py macgamestore_scrapper.py steam_store_scrapper.py
+mv *.db database_scrapping
