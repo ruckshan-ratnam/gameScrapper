5f3f7d50b1aae18c0f38d03583a9213ddeeb6b11
Ruckshan Ratnam <ruckshan@Ruckshans-MacBook-Pro.local>
2019-11-02 18:53:25 -0400

    added a script to make life easy

--- intil.sh
+++ intil.sh
@@ -2,12 +2,6 @@
 
 clear
 
-echo "moving databses and scrapping files to database and scrapping folder"
-
-mkdir database_scrapping
-mv -t database_scrapping gamebillet_scrapper.py gog_scrapper.py humble_bundle_scrapper.py info_collater.py macgamestore_scrapper.py steam_store_scrapper.py
-
-
 echo "making databases"
 
 python3 create_gamebillet_database.py & python3 create_gog_database.py & python3 create_humble_bundle_database.py python3 create_mac_game_store_database.py & python3 create_steam_database.py
